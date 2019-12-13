08e714f3c9cef65bb08347ec926e2cbf232f8e27
Tejaswin Parthasarathy <tp5@illinois.edu>
2019-12-12 20:15:41 -0600

    feat : KinematicSnake skeleton

--- kinematic_snake/kinematic_snake.py
+++ kinematic_snake/kinematic_snake.py
@@ -27,3 +27,98 @@ def zero_mean_integral(sampled_func, samples):
     return int_func_with_initial_values - trapz(
         int_func_with_initial_values, samples
     ).reshape(-1, 1)
+
+
+class KinematicSnake:
+    """
+    States are COM (x, y, theta, xdot, ydot, thetadot)
+    """
+
+    def __init__(
+        self, *, froude_number: float, friction_coefficients: dict, samples=200
+    ):
+        self.froude = froude_number if froude_number > 0.0 else -froude_number
+        self.curvature_activation = None
+        self.forward_mu = friction_coefficients.get("mu_f", 1.0)
+        self.backward_mu = friction_coefficients.pop("mu_b")
+        self.lateral_mu = friction_coefficients.pop("mu_lat")
+        self.samples = samples
+        self.centerline = np.linspace(0.0, 1.0, samples).reshape(1, -1)
+        self.zmi_along_centerline = partial(zero_mean_integral, samples=self.centerline)
+
+        # Set via
+        dofs = 3
+        self.state = np.zeros((dofs * 2,))
+
+    def set_activation(self, func):
+        def broadcast(fun):
+            """ Required because sp.diff of a constant function
+            returns by default a constant value (float) and not
+            an array of floats as one would expect.
+
+            Obtained from:
+            https://github.com/sympy/sympy/issues/5642#issuecomment-516419510
+
+            Parameters
+            ----------
+            fun
+
+            Returns
+            -------
+
+            """
+            return lambda *x: np.broadcast_arrays(fun(*x), *x)[0]
+
+        if func.__call__ is not None:
+            import sympy as sp
+
+            t, s = sp.symbols("t, s")
+            kappa = func(t, s)
+            kappa_rate = sp.diff(kappa, t)
+            kappa_acc = sp.diff(kappa_rate, t)
+            self.curvature_activation = broadcast(sp.lambdify([t, s], kappa, "numpy"))
+            self.dcurvature_activation_dt = broadcast(
+                sp.lambdify([t, s], kappa_rate, "numpy")
+            )
+            self.d2curvature_activation_dt2 = broadcast(
+                sp.lambdify([t, s], kappa_acc, "numpy")
+            )
+        else:
+            raise RuntimeError("Activation function not callable")
+
+    def _construct(self, time):
+
+        # First construct theta(s,t) and \frac{\partial X}{\partial s} from eq (2b)
+        theta_com = self.state[2]
+        self.theta_s = theta_com + self.zmi_along_centerline(
+            self.curvature_activation(time, self.centerline)
+        )
+        self.dx_ds = np.vstack((np.cos(self.theta_s), np.sin(self.theta_s)))
+
+        # The cross operator \perp of \frac{\partial X}{\partial s}
+        # Can do cross with [0., 0., 1], but why bother?
+        dx_ds_perp = 0.0 * self.dx_ds
+        dx_ds_perp[0, ...] = -self.dx_ds[1, ...]
+        dx_ds_perp[1, ...] = self.dx_ds[0, ...]
+
+        # Reconstruct x(s,t) from (2a)
+        x_com = self.state[:2].reshape(-1, 1).copy()
+        self.x_s = x_com + self.zmi_along_centerline(self.dx_ds)
+
+        # Get \frac{\partial \theta}{\partial t} from (3b)
+        theta_dot_at_com = self.state[5]
+        self.dtheta_dt = theta_dot_at_com + self.zmi_along_centerline(
+            self.dcurvature_activation_dt(time, self.centerline)
+        )
+
+        # Get \frac{\partial X}{\partial t} from (3a)
+        x_dot_at_com = self.state[3:5].reshape(-1, 1).copy()
+        self.dx_dt = x_dot_at_com + self.zmi_along_centerline(
+            dx_ds_perp * self.dtheta_dt
+        )
+
+    def non_dim_friction_force(self):
+        pass
+
+    def __call__(self, time, state, *args, **kwargs):
+        pass
