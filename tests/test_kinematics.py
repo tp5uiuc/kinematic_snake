46389ae41240fa3e15e5b21e0291574240cf57db
Tejaswin Parthasarathy <tp5@illinois.edu>
2020-02-22 13:01:17 -0600

    fmt : black + flake8 fixes

--- tests/test_kinematics.py
+++ tests/test_kinematics.py
@@ -175,7 +175,9 @@ class TestKinematicSnake:
         # 2 * (l/2)^3 / 3.0
         expected_moment_of_inertia = 2.0 * (1.0 / 2.0) ** 3 / 3.0
 
-        assert_allclose(moment_of_inertia, expected_moment_of_inertia, rtol=1e-4, atol=1e-4)
+        assert_allclose(
+            moment_of_inertia, expected_moment_of_inertia, rtol=1e-4, atol=1e-4
+        )
 
     def test_internal_torque(self, load_snake):
         snake, fc = load_snake
