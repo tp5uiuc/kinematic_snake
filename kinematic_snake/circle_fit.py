ad367998d374381f554842917aca412edc61eabb
Tejaswin Parthasarathy <tp5@illinois.edu>
2020-03-11 12:45:10 -0500

    feat : methods + tests to fit data to a circle

---
+++ kinematic_snake/circle_fit.py
@@ -0,0 +1,78 @@
+__doc__ = """ Fits a set of 2D data points (x,y) to a circle
+
+See https://scipy-cookbook.readthedocs.io/items/Least_Squares_Circle.html
+From https://dtcenter.org/met/users/docs/write_ups/circle_fit.pdf
+For more complicated shapes, see https://arxiv.org/pdf/cs/0301001.pdf
+"""
+
+import numpy as np
+from scipy.optimize import least_squares
+
+
+def fit_circle_to_data(in_data, verbose=False):
+    """
+    Wrapper around _fit_circle_impl that takes care of unwrapping shapes and so on...
+    Returns
+    -------
+
+    """
+    xc, yc, r, opt = circle_fit_impl(in_data)
+
+    if verbose:
+        print("Number of function evaluations : {}".format(opt.nfev))
+        print("Number of Jacobian evaluations : {}".format(opt.njev))
+        print("Stddev. residuals at optimal : {}".format(np.std(opt.fun)))
+
+    return xc, yc, r
+
+
+def circle_fit_impl(x):
+    """ (2,n) array as input
+    """
+
+    def distance_from_center(xc, yc):
+        """ calculate the distance of each 2D points from the center (xc, yc) """
+        return np.sqrt((x[0] - xc) ** 2 + (x[1] - yc) ** 2)
+
+    def objective(c):
+        """ calculates the objective : algebraic distance between the data points
+        and the mean circle centered at c=(xc, yc)
+
+        Maps from R^2 -> R^n where n is the number of points to map
+        """
+        Ri = distance_from_center(*c)
+        return Ri - Ri.mean()
+
+    def jacobian_objective(c):
+        """ Jacobian of above objective function
+        By definition from scipy J_{ij} = \partial f_{i}/ \partial x_{j}
+        Hence J is a (n,2) matrix below
+
+        Parameters
+        ----------
+        c
+
+        Returns
+        -------
+
+        """
+        xc, yc = c
+        df2b_dc = np.empty((x.shape[-1], c.shape[0]))
+
+        r_i = distance_from_center(xc, yc)
+        df2b_dc[..., 0] = (xc - x[0]) / r_i  # dR/dxc
+        df2b_dc[..., 1] = (yc - x[1]) / r_i  # dR/dyc
+        df2b_dc -= np.mean(df2b_dc, axis=0)
+
+        return df2b_dc
+
+    # estimate center via mean of data
+    center_estimate = np.mean(x, axis=-1)
+    optimum_results = least_squares(
+        objective, center_estimate, jac=jacobian_objective, method="lm"
+    )
+
+    center_optimized = optimum_results.x
+    radius_samples = distance_from_center(*center_optimized)
+    radius = np.mean(radius_samples)
+    return (*center_optimized, radius, optimum_results)
---
+++ tests/test_fit_circle.py
@@ -0,0 +1,74 @@
+import numpy as np
+from numpy.testing import assert_allclose
+import pytest
+
+from kinematic_snake.circle_fit import circle_fit_impl
+
+
+class TestCircleFit:
+    def test_circle_fit_for_circular_data(self):
+        center = np.array([[0.0], [0.0]])
+        radius = 1.0
+        theta = np.linspace(0.0, 2.0 * np.pi, 20, endpoint=False)
+        x = center + radius * np.vstack((np.cos(theta), np.sin(theta)))
+
+        xc, yc, r, _ = circle_fit_impl(x)
+
+        assert_allclose(center[0, 0], xc, atol=1e-8)
+        assert_allclose(center[1, 0], yc, atol=1e-8)
+        assert_allclose(radius, r)
+
+    def test_circle_fit_for_circular_data_generated_randomly(self):
+        center = np.random.randn(2, 1)
+        radius = np.random.rand()
+        theta = np.linspace(0.0, 2.0 * np.pi, 20, endpoint=False)
+        x = center + radius * np.vstack((np.cos(theta), np.sin(theta)))
+
+        xc, yc, r, _ = circle_fit_impl(x)
+
+        assert_allclose(center[0, 0], xc)
+        assert_allclose(center[1, 0], yc)
+        assert_allclose(radius, r)
+
+    def test_circle_fit_for_randomly_sampled_circular_data(self):
+        center = np.random.randn(2, 1)
+        radius = np.random.rand()
+        theta = 2.0 * np.pi * np.random.random_sample(10)
+        x = center + radius * np.vstack((np.cos(theta), np.sin(theta)))
+
+        xc, yc, r, _ = circle_fit_impl(x)
+
+        assert_allclose(center[0, 0], xc)
+        assert_allclose(center[1, 0], yc)
+        assert_allclose(radius, r)
+
+    def test_circle_fit_for_circular_like_data_with_different_radii(self):
+        center = np.array([[0.0], [0.0]])
+        n_samples = 20
+        variability = 0.05
+        # Radius is around 1, not exactly though
+        radius = 1.0 + variability * np.random.randn(n_samples)
+        theta = np.linspace(0.0, 2.0 * np.pi, n_samples, endpoint=False)
+        x = center + radius * np.vstack((np.cos(theta), np.sin(theta)))
+
+        xc, yc, r, _ = circle_fit_impl(x)
+
+        assert np.abs(center[0, 0] - xc) < variability
+        assert np.abs(center[1, 0] - yc) < variability
+        assert_allclose(np.mean(radius), r, rtol=1e-2)
+
+    @pytest.mark.xfail(
+        reason="The tolerance to set is unclear and hence it is allowed to fail"
+    )
+    def test_circle_fit_for_circular_like_data_with_different_centers(self):
+        n_samples = 20
+        variability = 0.03
+        center = np.random.randn(2, n_samples) * variability
+        radius = 0.5
+        theta = np.linspace(0.0, 2.0 * np.pi, n_samples, endpoint=False)
+        x = center + radius * np.vstack((np.cos(theta), np.sin(theta)))
+        xc, yc, r, _ = circle_fit_impl(x)
+
+        assert np.abs(center[0, 0] - xc) < variability
+        assert np.abs(center[1, 0] - yc) < variability
+        assert_allclose(np.mean(radius), r, rtol=1e-2)
