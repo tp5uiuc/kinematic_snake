:bug:fix : calculation of circle fit
With straight-line or near straight-line data, the fitting is not
robust (it tries to only minimize the least squared error). So before
fitting, we first run a linear-least squares to fit to a line (the
edge case of a circle with infinite radius), see if that fails to fit
and only then do the circle.

kinematic_snake/circle_fit.py
@@ -7,6 +7,7 @@ For more complicated shapes, see https://arxiv.org/pdf/cs/0301001.pdf
 
 import numpy as np
 from scipy.optimize import least_squares
+from scipy.stats import linregress
 
 
 def fit_circle_to_data(in_data, verbose=False):
@@ -16,14 +17,24 @@ def fit_circle_to_data(in_data, verbose=False):
     -------
 
     """
-    xc, yc, r, opt = circle_fit_impl(in_data)
+    slope, intercept, r_value, p_value, std_err = linregress(in_data[0], in_data[1])
 
     if verbose:
-        print("Number of function evaluations : {}".format(opt.nfev))
-        print("Number of Jacobian evaluations : {}".format(opt.njev))
-        print("Stddev. residuals at optimal : {}".format(np.std(opt.fun)))
+        print("R-square of linear fit : {}".format(r_value ** 2))
+        print("std-error of linear fit : {}".format(std_err))
 
-    return xc, yc, r
+    if r_value ** 2 < 0.98 and std_err > 1e-3:
+        xc, yc, r, opt = circle_fit_impl(in_data)
+
+        if verbose:
+            print("Number of function evaluations : {}".format(opt.nfev))
+            print("Number of Jacobian evaluations : {}".format(opt.njev))
+            print("Stddev. residuals at optimal : {}".format(np.std(opt.fun)))
+
+        return xc, yc, r
+    else:
+        # It's almost a straight line, so return all infinitirys
+        return np.inf, np.inf, np.inf
 
 
 def circle_fit_impl(x):

tests/test_fit_circle.py
@@ -2,7 +2,7 @@ import numpy as np
 from numpy.testing import assert_allclose
 import pytest
 
-from kinematic_snake.circle_fit import circle_fit_impl
+from kinematic_snake.circle_fit import circle_fit_impl, fit_circle_to_data
 
 
 class TestCircleFit:
@@ -72,3 +72,26 @@ class TestCircleFit:
         assert np.abs(center[0, 0] - xc) < variability
         assert np.abs(center[1, 0] - yc) < variability
         assert_allclose(np.mean(radius), r, rtol=1e-2)
+
+    def test_circle_fit_for_straight_line_data(self):
+        n_samples = 20
+        x = np.linspace(0.0, 1.0, n_samples)
+        y = 0.0 * x
+        position_vector = np.vstack((x, y))
+
+        xc, yc, r = fit_circle_to_data(position_vector, verbose=True)
+
+        assert r > 1e4
+
+    def test_circle_fit_for_almost_straight_line_data(self):
+        n_samples = 20
+        x = np.linspace(0.0, 1.0, n_samples)
+        y = 0.1 * x
+        y += 0.05 * np.random.random_sample(n_samples)
+        position_vector = np.vstack((x, y))
+
+        xc, yc, r = fit_circle_to_data(position_vector, verbose=True)
+
+        # Shady assert : do not know when it works or fails as
+        # the data is randomly sampled
+        assert r > 1